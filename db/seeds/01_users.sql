bb305b24771ea30570fe4bb79c6ec8455cb65775
JRam2022 <jasonram778@gmail.com>
2022-05-07 12:52:31 -0700

    Initial users seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,14 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Paulie Creagh', 'pcreagh0@simplemachines.org', 'password', '770-877-7891', 'pcreagh0', 2);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Bari Figgess', 'bfiggess1@jiathis.com', 'password', '377-115-2072', null, 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Salvador McCallam', 'smccallam2@wsj.com', 'password', '299-211-3776', 'smccallam2', 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Basilio Spours', 'bspours3@prlog.org', 'password', '175-766-8542', null, 3);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Humfrid Hugle', 'hhugle4@opera.com', 'password', '504-758-4944', 'hhugle4', 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Brittani Semerad', 'bsemerad5@about.me', 'password', '491-193-7027', null, 3);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Murielle Dullard', 'mdullard6@who.int', 'password', '678-704-8386', 'mdullard6', 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Jenny Ruoff', 'jruoff7@skype.com', 'password', '224-999-7961', null, 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Riane Chapleo', 'rchapleo8@livejournal.com', 'password', '616-882-8069', 'rchapleo8', 1);
+INSERT INTO users (name, email password, phone, instagram, role_id) VALUES ('Sybyl Dight', 'sdight9@biglobe.ne.jp', 'password', '521-473-6020', 'sdight9', 1);
+# Rows:
+10
+​
