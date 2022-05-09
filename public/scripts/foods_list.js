eec582626dba9898a5e2b5ebd105e524406fc858
IamHPark <heesoo.p1023@gmail.com>
2022-05-09 03:59:02 +0000

    update modal to change contents as it clicked

--- public/scripts/foods_list.js
+++ public/scripts/foods_list.js
@@ -1,13 +1,34 @@
 $(() => {
+  // create modal
+  const createModal = (data) => {
+    const $modal = $(`
+          <h3>${data.name}</h3>
+          <p>${data.description}</p>
+          <p>Price: ${data.price}</p>
+    `)
+    return $('.modal-content').append($modal);
+  };
+
   // open popup
   $(".food").on("click", function (event) {
     event.preventDefault();
     $(".modal").addClass("is-visible");
+
+    // get food.id that has been clicked
+    let elmId = $(this).attr("id");
+
+    $.ajax({
+      url: `/foods/${elmId}`,
+      method: 'GET',
+    }).then((foods) => {
+      createModal(foods);
+    })
   });
 
   //close popup
-  $(".close").on("click", function (event) {
+  $("#close").on("click", function (event) {
     event.preventDefault();
+    $('.modal-content').empty();
     $(".modal").removeClass("is-visible");
   });
 });
