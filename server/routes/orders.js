83fdfca91bdc2c794fb01b487d94ae72637d7ca1
JC Flores <tothenextcode@gmail.com>
2022-05-09 23:52:30 +0000

    Modify date values to return time only and style for better readability

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -8,12 +8,44 @@
 const express = require('express');
 const router  = express.Router();
 
+const formatTime = (date) => {
+
+  const options = {
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+
+  return new Intl.DateTimeFormat('en-CA', options).format(date);
+}
+
+const formatRemainingTime = (date) => {
+
+  const options = {
+    minute: '2-digit',
+  };
+
+  return new Intl.DateTimeFormat('en-CA', options).format(date);
+}
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.getAllOrdersNotPickedUp()
-      .then(data => {
+      .then(orders => {
         const orderStates = ['Pending', 'Preparing', 'Ready for Pickup'];
-        const templateVars = { orders: data, orderStates};
+
+        for (const order in orders) {
+          orders[order].remaining_time = formatRemainingTime(Math.abs(orders[order].pickup_time - orders[order].created_date));
+          orders[order].created_date = formatTime(orders[order].created_date);
+
+          if (orders[order].pickup_time) {
+            orders[order].pickup_time = formatTime(orders[order].pickup_time);
+          }
+          const names = orders[order].name.split(' ');
+
+          orders[order].abbrev_name = `${names[0]} ${names[1][0]}.`
+        }
+
+        const templateVars = { orders, orderStates};
         res.render('orders', templateVars);
       })
       .catch(err => {
